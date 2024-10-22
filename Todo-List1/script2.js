ead56563de4b41ebf7b4c4a7b057ae4737b9371e
Samyak-Nagrale-17 <samyak.nagrale@emergys.com>
2024-10-22 16:23:56 +0530

    pushing all local files

--- Todo-List1/script2.js
+++ Todo-List1/script2.js
@@ -2,10 +2,11 @@ const addTaskButton = document.getElementById("addTaskButton")
 const addTaskInput = document.getElementById("addTaskInput")
 const taskList = document.querySelector("ul.taskList")
 const clearAllButton = document.getElementById("deleteAllTasks")
-
-
+const inputValidation = /^[a-zA-Z1-9]/;
+const inputValidationCategory = /^[a-zA-Z]/;
 // arrayof json objects
 let taskArray = []
+
 // add new task
 function addNewTask() {
     let taskDesc = addTaskInput.value.trim()
@@ -13,13 +14,31 @@ function addNewTask() {
         alert("Cannot add an empty task ... \nPlease provide a task description ...")
         return
     }
+    else if(inputValidation.test(taskDesc) == false){
+        alert("Task Description should begin with alphanumeric characters ... \nPlease provide a valid task description ...")
+        return
+    }
+
+    let taskLabel = document.getElementById("addTaskInputCategory") 
+    if(taskLabel.value.trim() === ""){
+        alert("Category/label can't be empty ...")
+        return
+    }
+    else if(inputValidationCategory.test(taskLabel.value) == false){
+        alert("Category should begin with letters ... \nPlease provide a valid task category ...")
+        return
+    }
 
     const li = document.createElement("li")
     li.classList.add("newTask")
     // add icons
-    li.innerHTML = `
+    li.innerHTML = 
+    `
     <i class="fa-regular fa-circle-check tick"></i>
-    <span class="taskDesc">${taskDesc}</span>
+    <div class="spanWrapper">
+        <span class="taskDesc">${taskDesc}</span>
+        <span class="taskLabel"><strong>Category:</strong> ${taskLabel.value}</span>
+    </div>
     <i class="fa-regular fa-circle-xmark cross"></i>
     `;
 
@@ -31,6 +50,7 @@ function addNewTask() {
 
     //clear the input feild
     addTaskInput.value = ""
+
     // display filter and clear all as non-zero tasks
     // toggleClearAllButton()
 
@@ -39,9 +59,12 @@ function addNewTask() {
     let listJson = {
         taskId: taskId,
         taskDesc: taskDesc,
-        completed:false
+        completed:false,
+        taskLabel:taskLabel.value
     };
+    taskLabel.value= ""
 
+    // push new task in taskArray
     taskArray.push(JSON.stringify(listJson))
     console.log(`Following was pushed in localstorage: ${taskArray}`)
     
@@ -49,6 +72,7 @@ function addNewTask() {
     addEventListenerToTaskDesc()
     checkTickIcon()
     checkCrossIcon()
+    toggleClearAllButton()
 }
 
 
@@ -61,19 +85,19 @@ function checkTickIcon(){
         icon.addEventListener('click', markAsComplete)
     })
 }
-
 function markAsComplete(event){
     const li = event.target.parentElement
     const taskId = li.getAttribute("data-task-id")
     const tickIcon = li.querySelector("i.tick")
-
+    const taskLabel = li.querySelector("span.taskLabel")
     // check if already completed
     if(li.querySelector('.taskDesc').classList.contains('completed')){
         return
     }
     else{
         li.querySelector('.taskDesc').classList.add('completed')
-        tickIcon.classList.replace("fa-regular", "fa-solid")    
+        tickIcon.classList.replace("fa-regular", "fa-solid") 
+        taskLabel.classList.toggle("completed")   
     }
 
     // find this task in the  taskArray to update its status as completed
@@ -112,6 +136,7 @@ function removeTask(event){
         let taskObject = JSON.parse(taskString)
         return taskObject.taskId != taskId
     })
+    toggleClearAllButton()
     saveData()
 }
 
@@ -124,25 +149,32 @@ function addEventListenerToTaskDesc(){
     spans.forEach(span => {
         span.addEventListener('click', editTaskDescription)
     })
+
+    // add the same function in Category
+    // const spansCategory = document.querySelectorAll("li span.taskLabel")
+    // add the event listener to each of them
+    // spansCategory.forEach(span => {
+    //     span.addEventListener('click', editTaskDescription)
+    // })
 }
 
 function editTaskDescription(event){
     let li = event.target.parentElement
     const span = li.querySelector("span.taskDesc")
     // if task is marked as completed, user cannot edit it
-    if (span.classList.contains("completed")) {return} 
+    if (span.classList.contains("completed")) return 
     
     const currentDesc = span.textContent
     const input = document.createElement("input")
     input.type = "text"
     input.value = currentDesc
 
-    // style the input
+    // style the input same as the span
     const computedStyle = window.getComputedStyle(span);
-    input.style.width = computedStyle.width; // Set width
-    input.style.height = computedStyle.height; // Set height
-    input.style.fontSize = computedStyle.fontSize; // Match font size
-    input.style.padding = computedStyle.padding; // Match padding
+    input.style.width = computedStyle.width; 
+    input.style.height = computedStyle.height; 
+    input.style.fontSize = computedStyle.fontSize; 
+    input.style.padding = computedStyle.padding; 
     input.style.border = computedStyle.border; 
 
     li.insertBefore(input, span)
@@ -152,6 +184,13 @@ function editTaskDescription(event){
     
     input.addEventListener("blur", function () {
         const newDesc = input.value.trim() || currentDesc
+        
+        // do input validation on this new change
+        if(inputValidation.test(newDesc) == false){
+            alert("Please enter valid task description...")
+            input.focus() 
+            input.value = ""
+        }
         span.textContent = newDesc
         li.insertBefore(span, input)
         input.remove()
@@ -170,9 +209,7 @@ function editTaskDescription(event){
         }
         saveData()
     })
-
     saveData() 
-
 }
 
 // clear all tasks. remove all the tasks from the taskArray as well
@@ -182,69 +219,80 @@ function deleteAllTasks(){
         taskList.innerHTML = ""
         // clear the task array
         taskArray = []
+        toggleClearAllButton()
         saveData()
-    } 
+    }
     else{
         return
     }
 }
 
-
 // filter the task by their status.
-// Assuming you have the following buttons in your HTML
-let filterAllButton = document.getElementById("filterAll");
-let filterTodoButton = document.getElementById("filterTodo");
-let filterCompletedButton = document.getElementById("filterCompleted");
+let filterAllButton = document.getElementById("filterAll")
+let filterTodoButton = document.getElementById("filterTodo")
+let filterCompletedButton = document.getElementById("filterCompleted")
 
-// Add event listeners to the filter buttons
-filterAllButton.addEventListener('click', () => {filterTasks('all'); setActiveButton(filterAllButton);});
-filterTodoButton.addEventListener('click', () => {filterTasks('todo'); setActiveButton(filterTodoButton);});
-filterCompletedButton.addEventListener('click', () => {filterTasks('completed');setActiveButton(filterCompletedButton);});
+filterAllButton.addEventListener('click', () => {
+    filterTasks('all')
+    setActiveButton(filterAllButton)
+})
+
+filterTodoButton.addEventListener('click', () => {
+    filterTasks('todo') 
+    setActiveButton(filterTodoButton)
+})
+
+filterCompletedButton.addEventListener('click', () => {
+    filterTasks('completed')
+    setActiveButton(filterCompletedButton)
+})
 
 // Filter tasks based on the selected category
 function filterTasks(filterType) {
-    // Clear the current displayed tasks
     taskList.innerHTML = "";
 
-    // Filter the taskArray based on the filterType
     let filteredTasks = taskArray.filter(taskString => {
         let taskObject = JSON.parse(taskString);
         if (filterType === 'all') {
             return true; // Show all tasks
         } else if (filterType === 'todo') {
-            return !taskObject.completed; // Show only incomplete tasks
+            // todo i.e not completed
+            return !taskObject.completed
         } else if (filterType === 'completed') {
-            return taskObject.completed; // Show only completed tasks
+            // only completed
+            return taskObject.completed 
         }
         return false;
     });
 
-    // Reverse the filtered tasks
-    // if(filterType != "completed") 
-        filteredTasks = filteredTasks.reverse();
+    // reverse the filtered tasks
+    // find a better way to do this
+    filteredTasks = filteredTasks.reverse();
 
-    // Render the filtered tasks
+    // display the filtered tasks
     filteredTasks.forEach(taskString => {
         const taskObject = JSON.parse(taskString);
         const li = document.createElement('li');
         li.classList.add('newTask');
         li.setAttribute('data-task-id', taskObject.taskId);
 
-        // Determine the class for the tick icon based on completion status
+        // check if task is completed or not
         const completedClass = taskObject.completed ? "completed" : "";
-        const tickIconClass = taskObject.completed ? "fa-solid" : "fa-regular"; // Solid if completed, regular if not
-
+        const tickIconClass = taskObject.completed ? "fa-solid" : "fa-regular"; 
+        const taskLabel = taskObject.taskLabel ? taskObject.taskLabel.toString() : "Default" 
         li.innerHTML =
             `
                 <i class="${tickIconClass} fa-circle-check tick"></i>
-                <span class="taskDesc ${completedClass}">${taskObject.taskDesc}</span>
+                <div class="spanWrapper">
+                    <span class="taskDesc ${completedClass}">${taskObject.taskDesc}</span>
+                    <span class="taskLabel ${completedClass}"><strong>Category:</strong> ${taskLabel}</span>
+                </div>
                 <i class="fa-regular fa-circle-xmark cross"></i>
             `;
 
         taskList.appendChild(li);
     });
 
-    // Reattach event listeners after filtering
     // saveData()
     addEventListenerToTaskDesc();
     checkTickIcon();
@@ -263,6 +311,23 @@ function setActiveButton(activeButton) {
     activeButton.classList.add('active');
 }
 
+// hide the clear all and filter buttons if no tasks are present
+function toggleClearAllButton() {
+    let filterContainer = document.querySelector("div.filterContainer")
+    let buttonContainer = document.querySelector("div.buttonContainer")
+    // container and not button
+    //clearAllButton.style.display = taskList.children.length ? "block" : "none";
+   if(taskList.children.length === 0){
+        // clearAllButton.style.display = "none"
+        buttonContainer.style.display = "none"
+        filterContainer.style.display = "none"
+   }
+   else{
+        buttonContainer.style.display = "flex"
+        filterContainer.style.display = "flex"
+   }
+   return
+}
 
 // save the tasks in the local storage
 function saveData(){
@@ -278,7 +343,6 @@ function showData(){
         //reverse the taskArray
         // original taskArray is unchanged
         const tempTaskArray = Array.from(taskArray).reverse()
-        // console.log(taskArray)
         
         tempTaskArray.forEach(taskString => {
             const taskObject = JSON.parse(taskString)
@@ -288,17 +352,28 @@ function showData(){
             li.setAttribute('data-task-id', taskObject.taskId)
             const completedClass = taskObject.completed ? "completed" : ""
             const tickIcon = (completedClass == "completed") ? "fa-solid" : "fa-regular"
-
+            const taskLabel = taskObject.taskLabel ? taskObject.taskLabel.toString() : "Default"
+            const taskLabelCompleted = completedClass ? "completed" : "";
+        console.log("taskLabelCompleted value is (completed/)", taskLabelCompleted) 
             li.innerHTML = 
-            `<i class="${tickIcon} fa-circle-check tick"></i><span class="taskDesc ${completedClass}">${taskObject.taskDesc}</span><i class="fa-regular fa-circle-xmark cross"></i>`
+            `
+            <i class="${tickIcon} fa-circle-check tick"></i>
+            <div class="spanWrapper">
+                <span class="taskDesc ${completedClass}">${taskObject.taskDesc}</span>
+                <span class="taskLabel ${taskLabelCompleted}"><strong>Category:</strong> ${taskLabel}</span>
+            </div>
+            <i class="fa-regular fa-circle-xmark cross"></i>
+            `
 
             taskList.appendChild(li)
             addEventListenerToTaskDesc()
             checkTickIcon()
             checkCrossIcon()
+            toggleClearAllButton()
         })
-
     }
 }
 // document.addEventListener("DOMContentLoaded", showData)
+// when no tasks are there, hide the buttons.
+toggleClearAllButton()
 showData() 
